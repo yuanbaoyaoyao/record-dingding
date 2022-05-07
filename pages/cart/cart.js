c07caf4b89f13d57027d689f860afd32fd611866
yuanbaoyaoyao <583515921@qq.com>
2022-05-07 16:55:05 +0800

    cart

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,19 +1,48 @@
 Page({
-  data: {},
+  data: {
+    contentlist: [
+      {
+        "checkedpic": "/image/checked.svg",
+        "pic": "/image/mock/88a1.png",
+        "num": 2
+      },
+      {
+        "checkedpic": "/image/checked.svg",
+        "pic": "/image/mock/88a1.png",
+        "num": 2
+      },
+      {
+        "checkedpic": "/image/checked.svg",
+        "pic": "/image/mock/88a1.png",
+        "num": 2
+      },
+      {
+        "checkedpic": "/image/checked.svg",
+        "pic": "/image/mock/88a1.png",
+        "num": 2
+      },
+      {
+        "checkedpic": "/image/checked.svg",
+        "pic": "/image/mock/88a1.png",
+        "num": 2
+      },
+      {
+        "checkedpic": "/image/checked.svg",
+        "pic": "/image/mock/88a1.png",
+        "num": 2
+      },
+      {
+        "checkedpic": "/image/checked.svg",
+        "pic": "/image/mock/88a1.png",
+        "num": 2
+      },
+      {
+        "checkedpic": "/image/checked.svg",
+        "pic": "/image/mock/88a1.png",
+        "num": 2
+      },
+    ],
+  },
   onLoad() { },
-  onTabItemTap(item) {
-    let systemInfo = dd.getSystemInfoSync()
-    let screenHeight = dd.getSystemInfoSync().screenHeight
-    let windowHeight = dd.getSystemInfoSync().windowHeight
-    let statusBarHeight = dd.getSystemInfoSync().statusBarHeight
-    let pixelRatio = dd.getSystemInfoSync().pixelRatio
-    const tabbarHeight = (screenHeight - windowHeight - statusBarHeight) * pixelRatio
-    let tabbarWidth = dd.getSystemInfoSync().windowWidth
-    let cartHeight = tabbarHeight / 2
-    let cartWidth = tabbarWidth - tabbarWidth / 4
-    console.log("tabbarHeight:", tabbarHeight)
-    console.log("tabbarWidth:", tabbarWidth)
-    console.log("cartHeight:", cartHeight)
-    console.log("cartWidth:", cartWidth)
-  }
+
 });
