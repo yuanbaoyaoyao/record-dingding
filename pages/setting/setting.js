c889576c12fb6ad5d8113016538297fd725c2588
yuanbaoyaoyao <583515921@qq.com>
2022-05-12 10:34:27 +0800

    but fixes

--- pages/setting/setting.js
+++ pages/setting/setting.js
@@ -1,4 +1,24 @@
 Page({
   data: {},
-  onLoad() {},
+  onLoad() { },
+  handleToUserInfo() {
+    dd.navigateTo({
+      url: '/pages/setting/setting_details/user_info/user_info'
+    })
+  },
+  handleToAddressManagement() {
+    dd.navigateTo({
+      url: '/pages/setting/setting_details/address_management/address_management'
+    })
+  },
+  handleToPasswordManagement() {
+    dd.navigateTo({
+      url: '/pages/setting/setting_details/password_management/password_management'
+    })
+  },
+  handleToAboutMe() {
+    dd.navigateTo({
+      url: '/pages/setting/setting_details/address_management/address_management'
+    })
+  }
 });
--- pages/setting/setting_details/address_management/address_management.js
+++ pages/setting/setting_details/address_management/address_management.js
@@ -1,4 +1,25 @@
 Page({
-  data: {},
-  onLoad() {},
+  data: {
+    addressInfo: [
+      {
+        "name": "元宝尧尧",
+        "phone": "180******8983",
+        "region": "这是地区",
+        "detailAddress": "这是详细地址"
+      },
+      {
+        "name": "元宝尧尧",
+        "phone": "180******8983",
+        "region": "这是地区",
+        "detailAddress": "这是详细地址"
+      },
+      {
+        "name": "元宝尧尧",
+        "phone": "180******8983",
+        "region": "这是地区",
+        "detailAddress": "这是详细地址"
+      },
+    ]
+  },
+  onLoad() { },
 });
